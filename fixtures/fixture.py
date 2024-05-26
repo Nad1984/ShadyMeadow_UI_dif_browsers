89be844a5ee3deacfc166bdad6fa9d5d43ac2e9f
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2024-05-26 20:00:09 +0300

    changed                 if 'browser' in item.fixturenames:                     web_driver = item.funcargs['browser']

--- fixtures/fixture.py
+++ fixtures/fixture.py
@@ -13,7 +13,7 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption("--env", action="store", help='Env name to use: chrome, edge, firefox')
+    parser.addoption("--env", action="store", help='Env name to use: chrome, edge, firefox, headlesschrome, headlessfirefox')
 
 
 def get_browser(browser_name):
@@ -57,24 +57,47 @@ def shady_meadow_page(request, custom_logger):
     browser.quit()
 
 
-### FOR ALLURE report
-@pytest.hookimpl(hookwrapper=True)
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
-    report = outcome.get_result()
-    if report.when == "call":
-        xfail = hasattr(report, "wasxfail")
-        if (report.skipped and xfail) or (report.failed and not xfail):
+    rep = outcome.get_result()
+    if rep.when == 'call' and rep.failed:
+        mode = 'a' if os.path.exists('failures') else 'w'
+        try:
+            with open('failures', mode) as f:
+                if 'browser' in item.fixturenames:
+                    web_driver = item.funcargs['browser']
+                else:
+                    print('Fail to take screen-shot')
+                    return
+            allure.attach(
+                web_driver.get_screenshot_as_png(),
+                name='screenshot',
+                attachment_type=allure.attachment_type.PNG
+            )
+        except Exception as e:
+            print('Fail to take screen-shot: {}'.format(e))
 
-            is_frontend_test = True if 'shady_meadow_page' in item.fixturenames else False
-            if is_frontend_test:
-
-                results_dir = os.environ.get("RESULTS_DIR")
-                if not results_dir:
-                    raise Exception(f"Environment variable 'RESULTS_DIR' must be set.")
-                driver_fixture = item.funcargs['request']
-                allure.attach(driver_fixture.cls.browser.get_screenshot_as_png(),
-                              name='screenshot',
-                              attachment_type=allure.attachment_type.PNG)
+### FOR ALLURE report
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+    # outcome = yield
+    # report = outcome.get_result()
+    # if report.when == "call":
+    #     xfail = hasattr(report, "wasxfail")
+    #     if (report.skipped and xfail) or (report.failed and not xfail):
+    #
+    #         is_frontend_test = True if 'shady_meadow_page' in item.fixturenames else False
+    #         if is_frontend_test:
+    #
+    #             results_dir = os.environ.get("RESULTS_DIR")
+    #             if not results_dir:
+    #                 raise Exception(f"Environment variable 'RESULTS_DIR' must be set.")
+    #             driver_fixture = item.funcargs['request']
+    #             allure.attach(driver_fixture.cls.browser.get_screenshot_as_png(),
+    #                           name='screenshot',
+    #                           attachment_type=allure.attachment_type.PNG)
+    #             allure.attach()
 
 
